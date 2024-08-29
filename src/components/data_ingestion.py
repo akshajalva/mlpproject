071e9b0d5ba68e69132ae242b0b197e40fa9a66d
Akshaj Alva <akshajalva0@gmail.com>
2024-08-29 20:56:15 +0530

    Changes in ingestion file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,6 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
