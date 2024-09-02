1b2cab6e4c4647e51f53fa9bd9d65214aedd6033
Akshaj Alva <akshajalva0@gmail.com>
2024-09-02 12:50:56 +0530

    Commit Hyperparameters and predict pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,15 +45,54 @@ class ModelTrainer:
                 "XgBoost": XGBRegressor(),
                 "Gradient Boost Regressor": GradientBoostingRegressor(),
                 "ADA Boost Regressor": AdaBoostRegressor(),
-                "K Neighbours":KNeighborsRegressor(),
+                #"K Neighbours":KNeighborsRegressor(),
                 "Cat Boost Regressor": CatBoostRegressor()
                 }
             
+            
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boost Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XgBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Cat Boost Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "ADA Boost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+             }
+            
             logging.info("Model training initiated")
             
-            model_report:dict = evaluate_model(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models=models)
+            model_report:dict = evaluate_model(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models= models,param=params)
 
-            logging.info(f"Model training and evaluation completed{model_report}")
+            logging.info(f"Model evaluation completed{model_report}")
 
             best_model_score = max(model_report.values())
 
--- src/utils.py
+++ src/utils.py
@@ -25,19 +25,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
-        #   para = param[list(models.keys())[i]]
+            para = param[list(models.keys())[i]]
 
-        #   gs = GridSearchCV(model,cv=3)    
-        #   gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)    
+            gs.fit(X_train,y_train)
 
-        #   model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+       
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train) 
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -51,7 +52,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
       
     except Exception as e:
         CustomException
-        
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)        
 '''
 
 host = os.getenv('host')
